5bcdc565df824549e4e58e204299c99da505b6df
ashikmahmud <ashikmamud187@gmail.com>
2022-04-29 23:40:11 +0600

    install tailwind for working fast

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Header = () => {
-  return <Header>Header</Header>;
+  return (
+    <Header>
+      <div className="container mx-auto">
+        <nav>
+          <div className="log">Logo</div>
+          <ul className="">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Header>
+  );
 };
 
 export default Header;

a4795a0a73194b1441a57877203932f1dca92980
ashikmahmud <ashikmamud187@gmail.com>
2022-05-01 02:27:38 +0600

    now dynamic my teams part

--- src/Pages/Home/Teams/Teams.jsx
+++ src/Pages/Home/Teams/Teams.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 const Teams = () => {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://akanda-warehouse-server.herokuapp.com/teams`)
+      .then((res) => {
+        setTeams(res.data.data);
+      });
+  }, []);
   return (
     <div className="teams py-10">
       <div className="container">
@@ -11,79 +20,32 @@ const Teams = () => {
           </h2>
         </div>
         <div className="team-content mt-7 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  justify-between items-center gap-8">
-          <div className="team-member shadow-md rounded p-5">
-            <div className="image h-80 overflow-hidden rounded">
-              <img
-                className="w-full h-full object-cover"
-                src="http://webdesign-finder.com/rentiger/wp-content/uploads/2020/03/team-01.jpg"
-                alt="team1"
-              />
-            </div>
-            <div className="details text-center mt-3 bg-slate-100 py-4 rounded">
-              <span className="text-gray-500 my-2">Supplier</span>
-              <h3 className="text-2xl">Jhone Doe</h3>
-              <div className="socials flex justify-center gap-5 my-3 items-center">
-                <a href="/">
-                  <BsFacebook />
-                </a>
-                <a href="/">
-                  <BsTwitter />
-                </a>
-                <a href="/">
-                  <BsInstagram />
-                </a>
+          {teams?.map((team) => (
+            <div key={team._id} className="team-member shadow-md rounded p-5">
+              <div className="image h-80 overflow-hidden rounded">
+                <img
+                  className="w-full h-full object-cover"
+                  src={team?.avatar}
+                  alt="team1"
+                />
+              </div>
+              <div className="details text-center mt-3 bg-slate-100 py-4 rounded">
+                <span className="text-gray-500 my-2">{team?.designation}</span>
+                <h3 className="text-2xl">{team?.name}</h3>
+                <div className="socials flex justify-center gap-5 my-3 items-center">
+                  <a href="/">
+                    <BsFacebook />
+                  </a>
+                  <a href="/">
+                    <BsTwitter />
+                  </a>
+                  <a href="/">
+                    <BsInstagram />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="team-member shadow-md rounded p-5">
-            <div className="image h-80 overflow-hidden rounded">
-              <img
-                className="w-full h-full object-cover"
-                src="http://webdesign-finder.com/rentiger/wp-content/uploads/2020/03/team-01.jpg"
-                alt="team1"
-              />
-            </div>
-            <div className="details text-center mt-3 bg-slate-100 py-4 rounded">
-              <span className="text-gray-500 my-2">Supplier</span>
-              <h3 className="text-2xl">Jhone Doe</h3>
-              <div className="socials flex justify-center gap-5 my-3 items-center">
-                <a href="/">
-                  <BsFacebook />
-                </a>
-                <a href="/">
-                  <BsTwitter />
-                </a>
-                <a href="/">
-                  <BsInstagram />
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="team-member shadow-md rounded p-5">
-            <div className="image  h-80 overflow-hidden rounded">
-              <img
-                className="w-full h-full object-cover"
-                src="http://webdesign-finder.com/rentiger/wp-content/uploads/2020/03/team-01.jpg"
-                alt="team1"
-              />
-            </div>
-            <div className="details text-center mt-3 bg-slate-100 py-4 rounded">
-              <span className="text-gray-500 my-2">Supplier</span>
-              <h3 className="text-2xl">Jhone Doe</h3>
-              <div className="socials flex justify-center gap-5 my-3 items-center">
-                <a href="/">
-                  <BsFacebook />
-                </a>
-                <a href="/">
-                  <BsTwitter />
-                </a>
-                <a href="/">
-                  <BsInstagram />
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>

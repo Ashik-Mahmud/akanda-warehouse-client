8dd5d75d1248db9a5eadcd4900d7e88eedcbcfb7
ashikmahmud <ashikmamud187@gmail.com>
2022-04-30 19:45:24 +0600

    create product manage ment page design

--- src/Pages/ManageProducts/ManageProducts.jsx
+++ src/Pages/ManageProducts/ManageProducts.jsx
@@ -1,7 +1,126 @@
 import React from "react";
-
+import { BsTrash } from "react-icons/bs";
+import styled from "styled-components";
 const ManageProducts = () => {
-  return <div>ManageProducts</div>;
+  return (
+    <ManageProductsContainer className="p-5">
+      <div className="container">
+        <div className="title my-6">
+          <span>You can see all the products here</span>
+          <h2 className="text-2xl font-semibold">Manage All Products</h2>
+        </div>
+        <div className="table-wrapper bg-white p-10 border shadow-sm rounded">
+          <table className="w-full text-left">
+            <thead className="bg-gray-100">
+              <tr>
+                <th>ID</th>
+                <th width={300}>Name </th>
+                <th> Price </th>
+                <th> Stock </th>
+                <th> Supplier </th>
+                <th> Image </th>
+                <th width={120}> Status </th>
+                <th width={120}> Delete </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Laptop</td>
+                <td>443$ </td>
+                <td>434pcs</td>
+                <td>Ashik Mahmud</td>
+                <td>
+                  <img
+                    width={40}
+                    className="rounded border w-14"
+                    src="https://static-01.daraz.com.bd/p/9eb4281b7d9f084cec2d8e86fbc16259.jpg"
+                    alt=""
+                  />
+                </td>
+                <td>
+                  <span className="text-green-400">Active</span>
+                </td>
+                <td>
+                  <button className="bg-red-400 text-white p-2">
+                    <BsTrash />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Laptop</td>
+                <td>443$ </td>
+                <td>434pcs</td>
+                <td>Ashik Mahmud</td>
+                <td>
+                  <img
+                    width={40}
+                    className="rounded border w-14"
+                    src="https://static-01.daraz.com.bd/p/9eb4281b7d9f084cec2d8e86fbc16259.jpg"
+                    alt=""
+                  />
+                </td>
+                <td>
+                  <span className="text-green-400">Active</span>
+                </td>
+                <td>
+                  <button className="bg-red-400 text-white p-2">
+                    <BsTrash />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Laptop</td>
+                <td>443$ </td>
+                <td>434pcs</td>
+                <td>Ashik Mahmud</td>
+                <td>
+                  <img
+                    width={40}
+                    className="rounded border w-14"
+                    src="https://static-01.daraz.com.bd/p/9eb4281b7d9f084cec2d8e86fbc16259.jpg"
+                    alt=""
+                  />
+                </td>
+                <td>
+                  <span className="text-green-400">Active</span>
+                </td>
+                <td>
+                  <button className="bg-red-400 text-white p-2">
+                    <BsTrash />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="pagination flex justify-end gap-1 my-3 items-center">
+            <button className="p-2 px-4 bg-gray-100 bg-green-400 text-white">
+              1
+            </button>
+            <button className="p-2 px-4 bg-gray-100">2</button>
+            <button className="p-2 px-4 bg-gray-100">3</button>
+            <button className="p-2 px-4 bg-gray-100">4</button>
+            <select name="page" className="p-2 px-4 bg-gray-100" id="page">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </ManageProductsContainer>
+  );
 };
+
+const ManageProductsContainer = styled.section`
+  position: relative;
+  th,
+  td {
+    padding: 0.6rem;
+    border-bottom: 1px solid #f8f8f8;
+  }
+`;
 
 export default ManageProducts;

f624bc228ee9173de06424d56f786bebe0550461
ashikmahmud <ashikmamud187@gmail.com>
2022-05-02 02:51:25 +0600

    create dynamic statistic

--- src/Pages/Home/Statistic/Statistic.jsx
+++ src/Pages/Home/Statistic/Statistic.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import CountUp from "react-countup";
+import useProducts from "../../../Hooks/useProducts";
 const Statistic = () => {
+  const { products } = useProducts();
+
+  const totalMoney = products.reduce(
+    (acc, product) => Number(product.price) + acc,
+    0
+  );
+  const totalStock = products.reduce(
+    (acc, product) => Number(product.stockQty) + acc,
+    0
+  );
+
   return (
     <div className="py-10 md:p-20">
       <div className="container">
         <div className="statistic flex flex-col md:flex-row flex-wrap gap-6 items-center justify-between">
           <div className="text-center">
             <h2 className="num text-7xl md:text-8xl text-gray-200 font-bold">
-              <CountUp end={120} />
+              <CountUp end={products.length || 0} />
               pcs
             </h2>
             <span className="text-gray-500 font-medium uppercase -mt-6 block tracking-wide">
@@ -19,7 +31,7 @@ const Statistic = () => {
           </div>
           <div className="text-center">
             <h2 className="num text-7xl md:text-8xl  text-gray-200 font-bold">
-              $<CountUp end={34} />K
+              $<CountUp end={totalMoney || 0} />
             </h2>
             <span className="text-gray-500 font-medium uppercase -mt-6 block tracking-wide">
               Until Now
@@ -30,7 +42,7 @@ const Statistic = () => {
           </div>
           <div className="text-center">
             <h2 className="num text-7xl md:text-8xl text-gray-200 font-bold">
-              <CountUp end={345} />
+              <CountUp end={totalStock || 0} />
               pcs
             </h2>
             <span className="text-gray-500 font-medium uppercase -mt-6 block tracking-wide">
